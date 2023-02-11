73cbe3825fd99b93bfc76dfcf6eee33bf92cb344
kepich <pav007prikhod@mail.ru>
2023-02-12 00:10:28 +0700

    Spellbook button

--- character/MagicWidget.py
+++ character/MagicWidget.py
@@ -1,7 +1,7 @@
 import json
 
 from PyQt6 import QtGui
-from PyQt6.QtWidgets import QWidget, QTextEdit, QVBoxLayout, QLabel
+from PyQt6.QtWidgets import QWidget, QTextEdit, QVBoxLayout, QLabel, QScrollArea, QHBoxLayout, QPushButton
 
 
 class MagicWidget(QWidget):
@@ -15,12 +15,24 @@ class MagicWidget(QWidget):
 
         self.textEdit = QTextEdit()
         self.textEdit.setText(str(casts))
-        self.textEdit.setFixedWidth(600)
-        self.textEdit.setFixedHeight(400)
+
+        self.scrollAreaLeft = QScrollArea()
+        vLayoutL = QVBoxLayout()
+        self.scrollAreaLeft.setLayout(vLayoutL)
+
+        self.scrollAreaRight = QScrollArea()
+        vLayoutR = QVBoxLayout()
+        self.spellBookButton = QPushButton("Spell book")
+        vLayoutR.addWidget(self.spellBookButton)
+        vLayoutR.addWidget(self.textEdit)
+        self.scrollAreaRight.setLayout(vLayoutR)
 
         layout = QVBoxLayout()
         layout.addWidget(QLabel(name))
-        layout.addWidget(self.textEdit)
+        hLayout = QHBoxLayout()
+        hLayout.addWidget(self.scrollAreaLeft, stretch=2)
+        hLayout.addWidget(self.scrollAreaRight, stretch=1)
+        layout.addLayout(hLayout)
         self.setLayout(layout)
 
     def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
@@ -35,6 +47,3 @@ class MagicWidget(QWidget):
     def setData(self, data: dict):
         self.data = data["data"]
         self.textEdit.setText(data["data"])
-
-    def focus(self, a0: QtGui.QFocusEvent) -> None:
-        print("Focus out")
